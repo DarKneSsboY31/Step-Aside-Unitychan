8e4b371b11c4ed223b182050ddf3f823a7e13b01
DarKneSsboY31 <blackimpact0123@outlook.jp>
2018-09-22 21:00:54 +0900

    Step Aside Unitychan　初めてのコミット
    「Destroy Controller」に、Unityちゃんが通り過ぎたアイテムを削除するようにアタッチしました。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+    //Unityちゃんを定義
+    private GameObject unitychan;
+    //CarPrefabを定義
+    public GameObject CarPrefab;
+    //coinPrefabを定義
+    public GameObject coinPrefab;
+    //cornPrefabを定義
+    public GameObject cornPrefab;
+
+	// Use this for initialization
+	void Start () {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //CarPrefabのオブジェクトを取得
+        this.CarPrefab = GameObject.Find("CarPrefab");
+        //coinPrefabのオブジェクトを取得
+        this.coinPrefab = GameObject.Find("coinPrefab");
+        //cornPrefabのオブジェクトを取得
+        this.cornPrefab = GameObject.Find("TrafficConePrefab");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Unityちゃんのz軸がオブジェクトのz軸を超えた場合、オブジェクトを消す。
+        if (unitychan.transform.position.z  > this.transform.position.z +4.0f ) {
+            Destroy(gameObject);
+        }
+	}
+}
