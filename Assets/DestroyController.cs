572b73d62860772c8e04569b66db08f64b3e3af1
DarKneSsboY31 <blackimpact0123@outlook.jp>
2018-09-24 00:45:41 +0900

    Step Aside Unitychanプロジェクト ４回目のコミット
    「Destroy Controller」にて、無駄な関数、使っていないメンバ変数を取り除きました。

--- Assets/DestroyController.cs
+++ Assets/DestroyController.cs
@@ -6,23 +6,11 @@ public class DestroyController : MonoBehaviour {
 
     //Unityちゃんを定義
     private GameObject unitychan;
-    //CarPrefabを定義
-    public GameObject CarPrefab;
-    //coinPrefabを定義
-    public GameObject coinPrefab;
-    //cornPrefabを定義
-    public GameObject cornPrefab;
-
+    
 	// Use this for initialization
 	void Start () {
         //Unityちゃんのオブジェクトを取得
         this.unitychan = GameObject.Find("unitychan");
-        //CarPrefabのオブジェクトを取得
-        this.CarPrefab = GameObject.Find("CarPrefab");
-        //coinPrefabのオブジェクトを取得
-        this.coinPrefab = GameObject.Find("coinPrefab");
-        //cornPrefabのオブジェクトを取得
-        this.cornPrefab = GameObject.Find("TrafficConePrefab");
 
 	}
 	
