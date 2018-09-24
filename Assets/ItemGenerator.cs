cb674858910607cd0ec2638dd4e68d5ed222b0ca
DarKneSsboY31 <blackimpact0123@outlook.jp>
2018-09-25 03:28:23 +0900

    Step SIde Unitychanプロジェクト　５回目のコミット
    「Item Generator」の修正。同じ関数を使わずに出来るよう、書き換えました。

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -19,24 +19,19 @@ public class ItemGenerator : MonoBehaviour
     private float posRange = 3.4f;
 
     //アイテム発生場所 a
-    private int a = -210;
-    //アイテム発生場所 b
-    private int b = -160;
-    //アイテム発生場所 c
-    private int c = -110;
-    //アイテム発生場所 d
-    private int d = -60;
-    //アイテム発生場所 e
-    private int e = -10;
-    //アイテム発生場所 f
-    private int f = 40;
-    //アイテム発生場所 g
-    private int g = 90;
+    private int targetPos = -160;
 
+    // Use this for initialization
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
 
-    //アイテム呼び出し関数の作成
-    private void ItemGenerate(int Now, int Next) {
-        for (int i = Now; i < Next; i += 15)　　//ある地点からある地点の間でアイテムを一定間隔に生成
+    // Update is called once per frame
+    void Update()
+    {
+        if (unitychan.transform.position.z >= targetPos - 50 && targetPos < goalPos - 10)
         {
             //どのアイテムを出すのかをランダムに設定
             int num = Random.Range(1, 11);
@@ -46,7 +41,7 @@ public class ItemGenerator : MonoBehaviour
                 for (float j = -1; j <= 1; j += 0.4f)
                 {
                     GameObject cone = Instantiate(cornPrefab) as GameObject;
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, targetPos);
                 }
             }
             else
@@ -63,57 +58,19 @@ public class ItemGenerator : MonoBehaviour
                     {
                         //コインを生成
                         GameObject coin = Instantiate(coinPrefab) as GameObject;
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, targetPos + offsetZ);
                     }
                     else if (7 <= item && item <= 9)
                     {
                         //車を生成
                         GameObject car = Instantiate(CarPrefab) as GameObject;
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, targetPos + offsetZ);
                     }
                 }
             }
-        }
-    }
 
+            targetPos += 15;
 
-    // Use this for initialization
-    void Start()
-    {
-        //Unityちゃんのオブジェクトを取得
-        this.unitychan = GameObject.Find("unitychan");
-
-        //スタート時にaからbの位置にアイテムを生成
-        ItemGenerate(a, b);
-        }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if (unitychan.transform.position.z > a && unitychan.transform.position.z < a + 0.5f)       //場所aを通り過ぎた場合、bからcの位置でアイテム生成
-        {
-            ItemGenerate(b, c);
-  
-        }else if (unitychan.transform.position.z > b && unitychan.transform.position.z < b + 0.5f) //場所bを通り過ぎた場合、cからdの位置でアイテム生成
-        {
-            ItemGenerate(c, d);
-        }
-        else if (unitychan.transform.position.z > c && unitychan.transform.position.z < c + 0.5f) //場所cを通り過ぎた場合、dからeの位置でアイテム生成
-        {
-            ItemGenerate(d, e);
-        }
-        else if (unitychan.transform.position.z > d && unitychan.transform.position.z < d + 0.5f) //場所dを通り過ぎた場合、eからfの位置でアイテム生成
-        {
-            ItemGenerate(e, f);
-        }
-        else if (unitychan.transform.position.z > e && unitychan.transform.position.z < e + 0.5f) //場所eを通り過ぎた場合、fからgの位置でアイテム生成
-        {
-            ItemGenerate(f, g);
-        }
-        else if (unitychan.transform.position.z > f && unitychan.transform.position.z < f + 0.5f) //場所fを通り過ぎた場合、gからgoalPosの位置でアイテム生成
-        {
-            ItemGenerate(g, goalPos);
         }
 
     }
